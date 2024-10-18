8fb101f8e7e21290894a6f9dbd5a8e679cb50cb8
Vedant  Rathore <vedantrathore627@gmail.com>
2024-10-18 22:40:35 +0530

    Add files via upload

--- SERVER-SIDE/controllers/ProjectController.js
+++ SERVER-SIDE/controllers/ProjectController.js
@@ -283,7 +283,7 @@ const getMemberByproject_idNotInvolved = async (req, res) => {
         const userdata = res.locals.user;
         const {company_id} = userdata;
         const project_id = req.params.project_id
-        const result = await db.sequelize.query('select t.* from (select u.user_id , u.name , count(a.user_id) as ct from userinfos u left join assignments a on u.user_id = a.user_id WHERE u.role = 3 group by (u.user_id) order by a.user_id) t where t.user_id not in (select user_id from assignments where project_id = ? and company_id = ?)', {
+        const result = await db.sequelize.query('select t.* from (select u.user_id , u.name , count(a.user_id) as ct from userinfos u left join assignments a on u.user_id = a.user_id WHERE u.role = 3 and company_id = ? group by (u.user_id) order by a.user_id) t where t.user_id not in (select user_id from assignments where project_id = ? and company_id = ?)', {
             replacements: [project_id,company_id],
             type: QueryTypes.SELECT,
         })
